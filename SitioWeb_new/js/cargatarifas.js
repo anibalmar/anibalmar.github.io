4faecb7aa0ae08ca1e7da970d6fe9909db925d15
anibalmar <anibal.martinez@glerp.net.co>
2021-09-24 20:06:16 -0500

    Update cargatarifas.js

--- SitioWeb_new/js/cargatarifas.js
+++ SitioWeb_new/js/cargatarifas.js
@@ -13,7 +13,7 @@ recorrerTarifas2();
 function recorrerTarifas1() {
     const xhttp = new XMLHttpRequest();
 
-    xhttp.open('GET', './docs/tarifas.json', true);
+    xhttp.open('GET', '../docs/tarifas.json', true);
     xhttp.send();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
@@ -67,7 +67,7 @@ function recorrerTarifas1() {
 function recorrerTarifas2() {
     const xhttp = new XMLHttpRequest();
 
-    xhttp.open('GET', './docs/tarifas2.json', true);
+    xhttp.open('GET', '../docs/tarifas2.json', true);
     xhttp.send();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
